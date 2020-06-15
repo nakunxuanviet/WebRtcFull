fcf555072f1ea87df48dc218daa26a15dce72f34
PhuNguyen <nguyentanphu@hotmail.com>
2020-06-16 00:15:33 +0700

    Init local peer testing

--- WebRtcFull/Scripts/app.ts
+++ WebRtcFull/Scripts/app.ts
@@ -1,14 +1,58 @@
-﻿const constrains = {
-	video: true,
-	audio: true
-}
+﻿(async function () {
+	async function getMediaFromDevice() {
+		try {
+			return await navigator.mediaDevices.getUserMedia({
+				audio: true,
+				video: true
+			})
+		} catch (err) {
+			window.alert('You must enable video to use video call')
+			console.log(err)
+		}
+	}
+	const localVideo = document.querySelector('#localVideo') as HTMLVideoElement
+	const remoteVideo = document.querySelector('#remoteVideo') as HTMLVideoElement
 
-const selfVideo = document.querySelector('#self') as HTMLVideoElement
+	const localStream = await getMediaFromDevice()
+	localVideo.srcObject = localStream
 
-navigator.getUserMedia(constrains,
-	(stream) => {
-		selfVideo.src = window.URL.createObjectURL(stream)
-	}, (err) => {
-		window.alert('You must enable video to use video call')
-		console.log(err)
+	const serverConfig = null
+	const localPeerConnection = new RTCPeerConnection(serverConfig)
+	localStream.getTracks().forEach(track => {
+		localPeerConnection.addTrack(track, localStream)
 	})
+
+	const remotePeerConnection = new RTCPeerConnection(serverConfig)
+
+	localPeerConnection.onicecandidate = function (event) {
+		// adding candidate to remote peer connection
+		if (event.candidate) {
+			remotePeerConnection.addIceCandidate(event.candidate)
+			console.log(`Local ice candidate: ${event.candidate.candidate}`)
+		}
+	}
+
+	remotePeerConnection.onicecandidate = function (event) {
+		// adding candidate to local peer connection
+		if (event.candidate) {
+			localPeerConnection.addIceCandidate(event.candidate)
+			console.log(`Remote ice candidate: ${event.candidate.candidate}`)
+		}
+	}
+	remotePeerConnection.ontrack = function (event) {
+		remoteVideo.srcObject = event.streams[0]
+		console.log('Received remote stream')
+	}
+
+	// all set, create an offer
+	const localSessionDescription = await localPeerConnection.createOffer()
+	console.log(`Offer from localPeerConnection: ${localSessionDescription.sdp}`)
+	localPeerConnection.setLocalDescription(localSessionDescription)
+	remotePeerConnection.setRemoteDescription(localSessionDescription)
+
+	const remoteSessionDescription = await remotePeerConnection.createAnswer()
+	console.log(`Answer from remotePeerConnection: ${localSessionDescription.sdp}`)
+	localPeerConnection.setRemoteDescription(remoteSessionDescription)
+	remotePeerConnection.setLocalDescription(remoteSessionDescription)
+
+})()
--- WebRtcFull/wwwroot/js/app.js
+++ WebRtcFull/wwwroot/js/app.js
@@ -1,12 +1,65 @@
-const constrains = {
-    video: true,
-    audio: true
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-const selfVideo = document.querySelector('#self');
-navigator.getUserMedia(constrains, (stream) => {
-    selfVideo.src = window.URL.createObjectURL(stream);
-}, (err) => {
-    window.alert('You must enable video to use video call');
-    console.log(err);
-});
+(function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        function getMediaFromDevice() {
+            return __awaiter(this, void 0, void 0, function* () {
+                try {
+                    return yield navigator.mediaDevices.getUserMedia({
+                        audio: true,
+                        video: true
+                    });
+                }
+                catch (err) {
+                    window.alert('You must enable video to use video call');
+                    console.log(err);
+                }
+            });
+        }
+        const localVideo = document.querySelector('#localVideo');
+        const remoteVideo = document.querySelector('#remoteVideo');
+        const localStream = yield getMediaFromDevice();
+        localVideo.srcObject = localStream;
+        const serverConfig = null;
+        const localPeerConnection = new RTCPeerConnection(serverConfig);
+        localStream.getTracks().forEach(track => {
+            localPeerConnection.addTrack(track, localStream);
+        });
+        const remotePeerConnection = new RTCPeerConnection(serverConfig);
+        localPeerConnection.onicecandidate = function (event) {
+            // adding candidate to remote peer connection
+            if (event.candidate) {
+                remotePeerConnection.addIceCandidate(event.candidate);
+                console.log(`Local ice candidate: ${event.candidate.candidate}`);
+            }
+        };
+        remotePeerConnection.onicecandidate = function (event) {
+            // adding candidate to local peer connection
+            if (event.candidate) {
+                localPeerConnection.addIceCandidate(event.candidate);
+                console.log(`Remote ice candidate: ${event.candidate.candidate}`);
+            }
+        };
+        remotePeerConnection.ontrack = function (event) {
+            remoteVideo.srcObject = event.streams[0];
+            console.log('Received remote stream');
+        };
+        // all set, create an offer
+        const localSessionDescription = yield localPeerConnection.createOffer();
+        console.log(`Offer from localPeerConnection: ${localSessionDescription.sdp}`);
+        localPeerConnection.setLocalDescription(localSessionDescription);
+        remotePeerConnection.setRemoteDescription(localSessionDescription);
+        const remoteSessionDescription = yield remotePeerConnection.createAnswer();
+        console.log(`Answer from remotePeerConnection: ${localSessionDescription.sdp}`);
+        localPeerConnection.setRemoteDescription(remoteSessionDescription);
+        remotePeerConnection.setLocalDescription(remoteSessionDescription);
+    });
+})();
 //# sourceMappingURL=app.js.map
